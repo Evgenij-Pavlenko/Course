c45a4ffa93f6d4d81f8db59e2d8892bd2351e5df
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-22 18:11:38 +0100

    rxjs end(

--- Angular9/rxjs-youtube-course-master/src/problem.js
+++ Angular9/rxjs-youtube-course-master/src/problem.js
@@ -1,13 +1,27 @@
+import {interval} from 'rxjs'
+import {filter, map} from 'rxjs/operators'
+
 const btn = document.getElementById('interval')
 const rxjsBtn = document.getElementById('rxjs')
 const display = document.querySelector('#problem .result')
 
 const people = [
-  {name: 'Vladilen', age: 25},
-  {name: 'Elena', age: 17},
-  {name: 'Ivan', age: 18},
-  {name: 'Igor', age: 14},
-  {name: 'Lisa', age: 32},
-  {name: 'Irina', age: 23},
-  {name: 'Oleg', age: 20}
+    {name: 'Vladilen', age: 25},
+    {name: 'Elena', age: 17},
+    {name: 'Ivan', age: 18},
+    {name: 'Igor', age: 14},
+    {name: 'Lisa', age: 32},
+    {name: 'Irina', age: 23},
+    {name: 'Oleg', age: 20}
 ]
+
+rxjsBtn.addEventListener('click', () => {
+    interval(1000)
+        .pipe(
+            filter(v => people[v] >= 18),
+            map(v => people[v].name)
+        )
+        .subscribe(res => {
+            display.textContent = res
+        })
+})
