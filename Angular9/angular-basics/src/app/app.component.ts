6046f9633ea8890426f887156f47007d8f5b0b43
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-25 17:03:46 +0100

    FormsModule - add 2way binding

--- Angular9/angular-basics/src/app/app.component.ts
+++ Angular9/angular-basics/src/app/app.component.ts
@@ -6,27 +6,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
-  number = 37;
-  img = 'https://cdn-front.kwork.ru/pics/t3/92/4037249-1578731692.jpg';
-  inputValue = '';
+  title = 'InputValue';
 
-  constructor() {
-    setTimeout(() => {
-      this.img = 'https://pluralsight2.imgix.net/paths/images/angular-14a0f6532f.png';
-    }, 2000);
-  }
-
-  onInput(event: KeyboardEvent) {
-    console.log('Event:', event);
-    this.inputValue = ( <HTMLInputElement> event.target).value;
-  }
-
-  onClick() {
-    console.log('Click!');
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str;
+  onClick(event: any) {
+    this.title = event.target.value;
   }
 }
