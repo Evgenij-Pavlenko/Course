dd1653b6d1eae04a4a2d250b61ef393e3f991e1d
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-25 17:25:07 +0100

    ngStyle

--- Angular9/angular-basics/src/app/app.component.ts
+++ Angular9/angular-basics/src/app/app.component.ts
@@ -6,9 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'InputValue';
+  backgroundToggle = false;
 
-  onClick(event: any) {
-    this.title = event.target.value;
-  }
 }
