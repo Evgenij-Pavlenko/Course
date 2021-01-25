e67fdfec6e1c76a8fe44a1f27529bcbae01f1fba
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-25 01:31:59 +0100

    add more my Component "post"

--- Angular9/angular-basics/src/app/post/post.component.ts
+++ Angular9/angular-basics/src/app/post/post.component.ts
@@ -2,8 +2,8 @@ import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-post',
-  templateUrl: 'post.component.html',
-  styleUrls: ['post.component.css']
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
 })
 export class PostComponent {
 }
