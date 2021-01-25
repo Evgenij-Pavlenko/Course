d5c99ee25ea6330caa6f66b081599027697e03bd
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-25 15:01:37 +0100

    Event binding

--- Angular9/angular-basics/src/app/app.component.ts
+++ Angular9/angular-basics/src/app/app.component.ts
@@ -9,11 +9,24 @@ export class AppComponent {
   title = 'angular-basics';
   number = 37;
   img = 'https://cdn-front.kwork.ru/pics/t3/92/4037249-1578731692.jpg';
-
+  inputValue = '';
 
   constructor() {
     setTimeout(() => {
       this.img = 'https://pluralsight2.imgix.net/paths/images/angular-14a0f6532f.png';
     }, 2000);
+  }
+
+  onInput(event: KeyboardEvent) {
+    console.log('Event:', event);
+    this.inputValue = ( <HTMLInputElement> event.target).value;
+  }
+
+  onClick() {
+    console.log('Click!');
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str;
   }
 }
