cd967f496e2203f42b73d15ea6d96a2cef883459
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-26 01:25:10 +0100

    ngFor

--- Angular9/angular-basics/src/app/app.component.ts
+++ Angular9/angular-basics/src/app/app.component.ts
@@ -6,6 +6,23 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  toggle = false;
+  arr = [1, 1, 2, 3, 5, 8, 13];
+
+  obj = [
+    {
+      title: 'Post1', author: 'John', coments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'}
+      ]
+    },
+    {
+      title: 'Post2', author: 'John2', coments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'}
+      ]
+    }
+  ];
 
 }
