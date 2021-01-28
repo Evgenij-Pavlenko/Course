d185aa17bceafe94d0c504d9a3c047dcccc84b92
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-01-28 17:22:37 +0100

    parameter transfer

--- Angular9/angular-basics/src/app/app.component.ts
+++ Angular9/angular-basics/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {title: 'Title 1', text: 'text 1 fgfgfvfvfvfvfvvfv', id: 1},
+    {title: 'Title 2', text: 'text 2 fgfgfvfvfvfvfvvfv', id: 2}
+  ];
 
-  now: Date = new Date();
 }
