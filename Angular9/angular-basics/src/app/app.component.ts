000bdd34b8b1ac61cf64871ffdb91a64e6ea80ed
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-02 15:18:06 +0100

    add my structural directive

--- Angular9/angular-basics/src/app/app.component.ts
+++ Angular9/angular-basics/src/app/app.component.ts
@@ -1,41 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  posts: Post[] = [
-    {title: 'Title 1', text: 'text 1 fgfgfvfvfvfvfvvfv', id: 1},
-    {title: 'Title 2', text: 'text 2 fgfgfvfvfvfvfvvfv', id: 2}
-  ];
-
-  ngOnInit(): void {
-    // setTimeout(() => {
-    //   console.log('Timeout');
-    //   // при   changeDetection: ChangeDetectionStrategy.OnPush - изменения отслеживаются, когда меняется весь объект @Input() post: Post;
-    //   this.posts[0] = {
-    //     title: 'Changed!',
-    //     text: 'changed 2',
-    //     id: 1
-    //   };
-    // }, 4000);
-  }
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-    // console.log('New post', post);
-  }
-
-  removePost(id: number) {
-    this.posts = this.posts.filter(p => p.id !== id);
-  }
+export class AppComponent {
+  isVisible = true;
 }
