0ede457c6106dec10831b56dc66ff2d394e9c060
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-09 00:27:31 +0100

    add local service with ng command

--- Angular9/angular-basics/src/app/app.component.ts
+++ Angular9/angular-basics/src/app/app.component.ts
@@ -1,16 +1,22 @@
 import {Component} from '@angular/core';
 import {AppCounterService} from './services/app-counter.service';
+import {LocalCounterService} from './services/local-counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LocalCounterService]
 })
 export class AppComponent {
- appCounterService: AppCounterService;
+  appCounterService: AppCounterService;
+  localCounterService: LocalCounterService;
 
-  constructor(appCounterService: AppCounterService) {
+  constructor(
+    appCounterService: AppCounterService,
+    localCounterService: LocalCounterService) {
     this.appCounterService = appCounterService;
+    this.localCounterService = localCounterService;
   }
 
 }
