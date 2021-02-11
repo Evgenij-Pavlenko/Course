a9217ad6d0927f3f08e1b2ca6627c4b91aa609b1
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-11 23:49:35 +0100

    form validations

--- Angular9/angular-forms/src/app/app.component.ts
+++ Angular9/angular-forms/src/app/app.component.ts
@@ -1,11 +1,28 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    form: FormGroup;
 
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            email: new FormControl('', [Validators.email, Validators.required]),
+            password: new FormControl(null, [Validators.required, Validators.minLength(3)])
+        });
+    }
+
+
+    submit() {
+        if (this.form.valid) {
+            console.log('Form: ', this.form);
+            const formData = this.form.value;
+            console.log('form data: ', formData);
+        }
+    }
 }
 
