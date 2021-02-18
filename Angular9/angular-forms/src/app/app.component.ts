3c690555b1d1eb42268b16674b335eb92c1c5599
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-18 13:55:05 +0100

    create FormGroups

--- Angular9/angular-forms/src/app/app.component.ts
+++ Angular9/angular-forms/src/app/app.component.ts
@@ -11,8 +11,17 @@ export class AppComponent implements OnInit {
 
     ngOnInit(): void {
         this.form = new FormGroup({
-            email: new FormControl('', [Validators.email, Validators.required]),
-            password: new FormControl(null, [Validators.required, Validators.minLength(3)])
+            email: new FormControl('', [
+                Validators.email,
+                Validators.required]),
+            password: new FormControl(null, [
+                Validators.required,
+                Validators.minLength(3)]),
+            address: new FormGroup({
+                country: new FormControl('ua'),
+                city: new FormControl('Kyiv', Validators.required)
+            })
+
         });
     }
 
