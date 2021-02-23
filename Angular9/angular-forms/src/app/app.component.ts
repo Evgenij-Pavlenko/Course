d5feb5fd81a06e79ffd9c303b951f1eee640bb58
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-23 17:10:44 +0100

    add acync Validator

--- Angular9/angular-forms/src/app/app.component.ts
+++ Angular9/angular-forms/src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent implements OnInit {
             email: new FormControl('', [
                 Validators.email,
                 Validators.required,
-            MyValidators.restrictedEmails
-            ]),
+                MyValidators.restrictedEmails
+            ], MyValidators.uniqEmail),
             password: new FormControl(null, [
                 Validators.required,
                 Validators.minLength(3)]),
