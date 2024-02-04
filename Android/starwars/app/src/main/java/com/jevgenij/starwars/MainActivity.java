7cb0ef918d687a80d00e642b0ea6522bc9c2c07e
Ievgenij Pavlenko <evgenij.pavlenko@gmail.com>
2024-02-05 00:24:07 +0100

    review animate - update

--- Android/starwars/app/src/main/java/com/jevgenij/starwars/MainActivity.java
+++ Android/starwars/app/src/main/java/com/jevgenij/starwars/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isAnakinVisible = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +21,15 @@ public class MainActivity extends AppCompatActivity {
         ImageView iv_anakin = findViewById(R.id.iv_anakin);
         ImageView iv_dartWader = findViewById(R.id.iv_dartWader);
 
-        if(iv_anakin.getAlpha() == 0){
-            iv_dartWader.animate().alpha(0).setDuration(2000);
-            iv_anakin.animate().alpha(1).setDuration(2000);
-        }else if(iv_dartWader.getAlpha() == 0){
+        if(isAnakinVisible){
             iv_dartWader.animate().alpha(1).setDuration(2000);
             iv_anakin.animate().alpha(0).setDuration(2000);
+            isAnakinVisible = false;
+
+        }else{
+            iv_dartWader.animate().alpha(0).setDuration(2000);
+            iv_anakin.animate().alpha(1).setDuration(2000);
+            isAnakinVisible = true;
         }
-
-
     }
 }
