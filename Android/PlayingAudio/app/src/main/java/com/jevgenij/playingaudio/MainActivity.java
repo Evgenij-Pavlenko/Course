af30a24009169b66b8598ad023843958deed50ab
Ievgenij Pavlenko <evgenij.pavlenko@gmail.com>
2024-05-29 14:09:21 +0200

    add volume regulator

--- Android/PlayingAudio/app/src/main/java/com/jevgenij/playingaudio/MainActivity.java
+++ Android/PlayingAudio/app/src/main/java/com/jevgenij/playingaudio/MainActivity.java
@@ -1,24 +1,64 @@
 package com.jevgenij.playingaudio;
 
+import static com.jevgenij.playingaudio.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     Button btn;
 
+    SeekBar volumeSeekBar;
+    AudioManager audioManager;
+
+    TextView tvVolume;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mediaPlayer = MediaPlayer.create(this, R.raw.stuff);
-        btn = findViewById(R.id.btn);
+        setContentView(layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, raw.stuff);
+        btn = findViewById(id.btn);
+        tvVolume = findViewById(R.id.tv_Volume);
+
+        tvVolume.setText("0 %");
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volumeSeekBar = findViewById(id.volumeSeekBar);
+        volumeSeekBar.setMax(maxVolume);
+
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("Progress volume change: ", "" + progress);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                tvVolume.setText("" + progress +" %");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
